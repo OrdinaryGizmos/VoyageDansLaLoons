97ed8758e887e6ecedef4b0e588e2b6cad6e55fd
Jonny <J.Kenyon@ordinarygizmos.com>
2024-01-28 17:20:44 -0600

    Remove manual controls

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -3,6 +3,6 @@ extends Node
 # make this a child of the thing you want to control!
 @onready var parent:Node2D = get_parent()
 
-func _physics_process(_delta: float) -> void:
-	parent.thrust_input = Input.get_axis("back", "forward")
-	parent.turn_input = Input.get_axis("left", "right")
+# func _physics_process(_delta: float) -> void:
+# 	parent.thrust_input = Input.get_axis("back", "forward")
+# 	parent.turn_input = Input.get_axis("left", "right")
